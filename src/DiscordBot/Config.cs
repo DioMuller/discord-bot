1bdca121550a8977ce0122dd9f2773be86d5f5f4
Diogo Muller <diogo.muller@live.com>
2021-06-03 21:03:05 -0300

    Add and Remove channel commands.

--- src/DiscordBot/Config.cs
+++ src/DiscordBot/Config.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace DiscordBot
@@ -11,6 +13,12 @@ namespace DiscordBot
         public string Token { get; set; }
 
         [JsonPropertyName("emoteChannelIds")]
-        public ulong[] EmoteChannelIds { get; set; }
+        public List<ulong> EmoteChannelIds { get; set; }
+
+        public void Save()
+        {
+            var text = JsonSerializer.Serialize<Config>(this);
+            File.WriteAllText("config.json", text);
+        }
     }
 }
--- src/DiscordBot/Modules/BotModule.cs
+++ src/DiscordBot/Modules/BotModule.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using Discord.WebSocket;
 using DiscordBot.Services;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,40 @@ namespace DiscordBot.Modules
     {
         public BotService BotService { get; set; }
 
+        public Config Config { get; set; }
+
         [Command("name")]
         public Task NameAsync([Remainder] string name)
             => BotService.ChangeName(name);
+
+        [Command("add_channel")]
+        public async Task AddChannel(SocketChannel channel)
+        {
+            if (!Config.EmoteChannelIds.Contains(channel.Id))
+            {
+                Config.EmoteChannelIds.Add(channel.Id);
+                Config.Save();
+                await ReplyAsync($"Added Channel {channel.Id}");
+            }
+            else
+            {
+                await ReplyAsync($"Channel {channel.Id} was already on the channel list.");
+            }
+        }
+
+        [Command("remove_channel")]
+        public async Task RemoveChannel(SocketChannel channel)
+        {
+            if (Config.EmoteChannelIds.Contains(channel.Id))
+            {
+                Config.EmoteChannelIds.Remove(channel.Id);
+                Config.Save();
+                await ReplyAsync($"Removed Channel {channel.Id}");
+            }
+            else
+            {
+                await ReplyAsync($"Channel {channel.Id} was not on the channel list.");
+            }
+        }
     }
 }
